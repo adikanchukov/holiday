d28911a14a77bbc913f7195556c2b22af423e48a
Alexander Dikanchukov <dikanchukov@mail.ru>
2016-01-05 15:45:17 +0300

    Added outputer

---
+++ HolidayOutputer.cpp
@@ -0,0 +1,6 @@
+#include "HolidayOutputer.h"
+
+HolidayOutputer::HolidayOutputer()
+{
+
+}
---
+++ HolidayOutputer.h
@@ -0,0 +1,11 @@
+#ifndef HOLIDAYOUTPUTER_H
+#define HOLIDAYOUTPUTER_H
+
+
+class HolidayOutputer
+{
+public:
+    HolidayOutputer();
+};
+
+#endif // HOLIDAYOUTPUTER_H
